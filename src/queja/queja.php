fdd112f136b92e0e3a05e36159a1bc5134ffc377
Abraham Rodarte España <abam_espana@hotmail.com>
2018-09-29 15:46:13 -0500

    Página de publicación de quejas y de sugerencias

---
+++ src/queja/queja.php
@@ -0,0 +1,12 @@
+<body>
+	<h2>Pubicar Queja</h2>
+	<form>
+		
+	<input type="text" name="tituloQueja" class="input" placeholder="Titulo" size="80">
+	<br><br>
+	<textarea class="input" placeholder="Descripción" ></textarea>
+	<br><br>
+	<input type="submit" name="">
+	
+	</form>
+</body>
