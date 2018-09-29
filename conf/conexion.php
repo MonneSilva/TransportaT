0ea2ed74dff5c59e174a0900c679bb4e89d87319
Abraham Rodarte España <abam_espana@hotmail.com>
2018-09-29 17:53:36 -0500

    Se añadió conexion.php

---
+++ conf/conexion.php
@@ -0,0 +1,19 @@
+<?php
+
+	
+	$enlaceInicial = mysqli_connect("localhost","root","","transportat");
+    if($enlaceInicial->connect_errno) {
+        printf("Connect failed: %s\n", mysqli_connect_error());
+        exit();
+    }
+
+    // Poder ver los warnings, '0' <=> '1'
+    error_reporting(E_ALL);
+    ini_set('display_errors', '1');
+
+    // Definir el juego de carácteres con el que se trabaja
+    $charset = "utf8";
+    mysqli_set_charset ($enlaceInicial , $charset);
+
+
+?>
