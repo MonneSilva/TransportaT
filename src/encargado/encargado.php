ae583b9f1500330939b0033ea00e8462ccb18328
marijosebi <40366076+marijosebi@users.noreply.github.com>
2018-09-29 19:57:20 -0500

    Add files via upload

---
+++ src/encargado/encargado.php
@@ -0,0 +1,30 @@
+<!DOCTYPE>
+
+<html>    <!--Poner el metacharset><-->
+
+<head><meta  charset="utf-8">
+<title>Encargado</title>
+
+</head>
+<body>
+
+
+    <section id="preferencias">
+    	<h4>Asignar preferencias </h4>
+     <div id="preferencias"></div>
+     <input type="text" id="campo" name="user" value="Nombre de inicio"><br><br>
+     <input type="text" id="campo" name="id_user" value="Id de usuario ">
+
+      </section>
+
+	<section id="verQuejas">
+		<h4>Ver Quejas </h4>
+      <button>Por unidad</button>
+      <button>Por operador</button>
+
+	</section>
+	
+
+	
+</body>
+</html>	
---
+++ src/registro/registro.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>    <!--Poner el metacharset><-->
+
+<head><meta  charset="utf-8">
+<title>Registro</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+	<section calss="formulario">
+	<form action=" ">
+
+		<p id="titulo">REGISTRO </p>  </div>
+    
+       Nombre: <input type="Text" class="input" name="im" placeholder="Nombre de usuario"> 
+   
+        Contraseñ <input type="Text" class="input" name="" placeholder="Contraseña"> 
+  
+        No. tarjeta <input type="Text" class="input" name="" placeholder="No. de tarjeta "> 
+        	Id. generado <label id="lbl_id" value=" hola funciono">Hola</label>
+
+    	<input type="submit" class="sumbit" name=" " value="Registrarme">
+</form>	
+ </section>
+
+
+</body>
+
+</html>
