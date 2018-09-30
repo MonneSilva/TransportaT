92b0cc5b986428f6574602abcec0d656e6e1d90f
marijosebi <40366076+marijosebi@users.noreply.github.com>
2018-09-30 00:51:59 -0500

    Add files via upload

---
+++ src/registro/registro.php
@@ -0,0 +1,23 @@
+<body>
+	
+
+  <section calss="formulario">
+  
+	<form class= "formsq" id="registro" action=" ">
+       
+		<h2 id="titulo">REGISTRO </h2> 
+        <input type="Text" class="campo" name=""  id="nombre-usuario" name="nombre-usuario" placeholder="Nombre de usuario"> <br>
+         <input type="Text" class="campo" name=""  id="password" name="password" placeholder="Contraseña"> <br>
+         <input type="Text" class="campo" name=""  id="tarjeta"  name="tarjeta" placeholder="No.de tarjeta "> <br>
+         <label campo id="lbl_id" class="campo" value=" hola funciono">Aqui va el id</label>
+         <br><br>  
+        
+       
+    	<input type="submit" class="submit" id="registrar" name="registrar" value="Registrar">
+   
+</form>	
+ </section>
+
+
+
+</body>
