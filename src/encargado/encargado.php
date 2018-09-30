4c304b8e6ad3f6166d8d10dbfc41ce48d1265ccc
marijosebi <40366076+marijosebi@users.noreply.github.com>
2018-09-30 01:00:48 -0500

    Add files via upload

---
+++ src/encargado/encargado.php
@@ -0,0 +1,25 @@
+
+<body>
+
+    <div  class="formqs">
+     <h2>Asignar preferencias </h2>
+   
+     <input type="Text" class="campo"   id="nombre-usuario" name="nombre-usuario" placeholder="Nombre de usuario"> <br><br>
+     <input type="Text" class="campo"   id="id-usuario" name="id-usuario" placeholder="ID. Usuario"> <br><br>
+     <input type="submit" class="asignar" name="registrar" value="Asignar">
+   </div>
+<br><br>
+	
+  <div  class="formqs">
+		<h2>Ver Quejas </h2><br><br>
+      <button class="submit" id="botone">Por unidad</button>  <br><br>
+      <button class="submit" id="botone">Por operador</button>
+   </div>
+
+	
+
+
+
+</body>
+	
+
