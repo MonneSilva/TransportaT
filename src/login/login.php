88934047d131e8fccfbc61fe7c059b228edc3920
Jassiel Hernández <39960127+jass1302@users.noreply.github.com>
2018-09-30 05:19:43 -0500

    Add files via upload

--- src/login/login.php
+++ src/login/login.php
@@ -1,21 +1,27 @@
+<?php
+  if (!isset($_SESSION['usuario'])) {
+
+  }
+  else{header("Location: index.php");}
+?>
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">
         <title>Iniciar sesión</title>
-        <script src="js/jquery-3.3.1.js"></script>
+        
         <link rel="stylesheet" type="text/css" href="css/login.css">
-        <!-- Para mostrar mensajes flash bonitos -->
+        
     </head>
     <body>
         <div class="" id="main-container">
           
-            <form id="acceso" action="" class="" method="post">
-                <input type="email"  id="userAcceso" name="userAcceso" class="campo" placeholder="Nombre Usuario" autofocus maxlength="65" required>
-                <input type="password" name="passAcceso" id="passAcceso" class="campo" placeholder="Password" maxlength="85" required>
+            <form id="acceso" action="validacion.php" method="POST">
+                <input type="email"  name="user" class="campo" placeholder="Nombre Usuario" autofocus maxlength="65" required>
+                <input type="password" name="pass" class="campo" placeholder="Password" maxlength="85" required>
                 <div class="" id="control">
                     <div id="boton">
-                        <input type="submit" id="btn-sign-in" name="btn-sign-in" value="Iniciar sesión">
+                        <button type="submit" id="btn-sign-in" name="btn-sign-in"> Iniciar Sesion</button>
                     </div>
                 </div>
             </form>
